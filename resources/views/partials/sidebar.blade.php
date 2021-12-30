c8667d7d2c5395847cf08ab333f74d38fce3e3aa
daur-vip <mail@daur.vip>
2021-12-30 11:17:24 +0600

    added if to show admin menu

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,35 +1,36 @@
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
 
-  <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
-    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">
-        <svg class="nav-icon">
-          <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
-        </svg> Dashboard</a></li>
+    <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
+        <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">
+                <svg class="nav-icon">
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
+                </svg> Dashboard</a></li>
 
-    <li class="nav-title">Admin</li>
-    <li class="nav-group">
-      <a class="nav-link" 
-      href="{{ route('admin.pages.index') }}">
-        <svg class="nav-icon">
-          <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
-        </svg>Pages
-      </a>
 
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+        @if(auth()->user()->is_admin)
+        <li class="nav-title">Admin</li>
+        <li class="nav-group">
+            <a class="nav-link" href="{{ route('admin.pages.index') }}">
+                <svg class="nav-icon">
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
+                </svg>Pages
+            </a>
+        </li>
+        @endif
+       
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
               document.getElementById('logout-form').submit();">
-        <svg class="nav-icon">
-          <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-account-logout') }}"></use>
-        </svg> {{ __('Logout') }}
-      </a>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-        @csrf
-      </form>
-    </li>
+                <svg class="nav-icon">
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-account-logout') }}"></use>
+                </svg> {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </li>
 
 
-  </ul>
-  <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
+    </ul>
+    <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
 </div>
